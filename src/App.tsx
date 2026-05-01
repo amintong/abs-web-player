9ed3292a856c11f1451da0847fa6a52a65678bac
amintong <414212523@qq.com>
2026-05-01 14:59:45 +0800

    fix: bottom space - use calc(0px - env(safe-area-inset-bottom)) on all fixed layers to extend to physical screen edge

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function ProtectedRoutes() {
         top: 0,
         left: 0,
         right: 0,
-        bottom: 0,
+        bottom: 'calc(0px - env(safe-area-inset-bottom))',
         paddingTop: 'env(safe-area-inset-top)',
       }}
     >
